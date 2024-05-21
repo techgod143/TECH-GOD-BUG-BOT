4f05c1a59b2490095cfe5e02608a6fc0c9e2f3d2
Tech God <106016855+techgod143@users.noreply.github.com>
2024-05-21 09:00:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -39,7 +39,7 @@ global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
 global.autorecordtype = true //auto typing + recording
 global.autoread = false //auto read messages
-global.autobio = true //auto update bio
+global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
 global.autoswview = true //auto view status/story
 
