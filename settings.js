f00a8e553c831c249f7072ae233ab53d1010d645
Shahzada906 <shahzadab117@gmail.com>
2024-08-06 23:29:23 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,9 +16,9 @@ global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
-global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
+global.ownernumber = '923163154307'  //creator number
+global.ownername = '🐛šɧɑ͢ɧᵶɑ͢D̷̅ɑ͢' //owner name
+global.botname = 'šɧɑ͢ɧᵶɑ͢D̷̅ɑ͢ вʊg͢  в๏ƚ' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
